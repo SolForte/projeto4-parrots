d20d32170c6600bc0a352b99eda1954bb2db826e
SolForte <mateusbrandao1234@gmail.com>
2023-01-13 17:00:58 -0300

    Card Nº Prompt bug fix

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ let card_quantity = 0;
 let first_flip, second_flip = undefined;
 function card_quantity_prompt(){
     card_quantity = Number(prompt("Com quantas cartas você jogar?"));
-    if (card_quantity < 4 || card_quantity > 14 || card_quantity % 2 || isNaN(card_quantity)){
-        card_quantity_prompt();
+    while (card_quantity < 4 || card_quantity > 14 || card_quantity % 2 || isNaN(card_quantity)){
+        card_quantity = Number(prompt("Com quantas cartas você jogar?"));
     }
     create_cards();
 }
@@ -51,18 +51,6 @@ function create_cards(){
     }
 }
 function card_flip(current_card) {
-    /*
-    If the user has an itchy finger, they might unintentionally trigger a bug if they click too fast
-    Bug Description:
-        -card_unflip (called by setTimout when the cards don't match) takes 1 second to happen due
-        to setTimeout. As such, if the user calls the function again before setTimeout(card_unflip,1000)
-        ends by itself: undesired things happens because the cards still aren't undefined yet to asign
-        current_card value to them.
-    Bug Fix & What the fix does:
-        -At the start of the function, create a conditional to make it return; if both cards variables
-        have values. That guarantees the function will only progress to it other statements if both cards
-        variables are "empty" (that happens after setTimeout(card_unflip,1000)).
-    */
     if (current_card.classList.contains("flip")) {
       return;
     }
@@ -78,8 +66,6 @@ function card_flip(current_card) {
         second_flip = current_card;
   
         if (first_flip.innerHTML === second_flip.innerHTML) {
-        //BUG: Apparently wrong syntax?
-        //Solution: Stop trying to be smart and do the usual way.
           first_flip = undefined;
           second_flip = undefined;
         } else {
@@ -91,7 +77,6 @@ function card_flip(current_card) {
 function card_unflip() {
     first_flip.classList.remove("flip");
     second_flip.classList.remove("flip");
-    //Same bug, fix...
     first_flip = undefined;
     second_flip = undefined;
 }
